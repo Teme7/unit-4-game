1ab870c0d340ed4443c2e0e9ca969765530a1fd2
Timmy <123.tsa@gmail.com>
2018-08-02 13:50:19 -0400

    16th push

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -2,29 +2,62 @@
 var randomNum;
 var wins = 0;
 var losses = 0;
-// var oldValue = 0;
+var oldValue = 0;
 
-randomNum = Math.floor(Math.random() * 100) + 19;
-// console.log(randomNum);
+var startGame = function (){
+    randomNum = Math.floor(Math.random() * 100) + 19;
+    // console.log(randomNum);
 
-$("#tally").html('randomNum: ' + randomNum);
-for(var i = 0; i < 4; i++){
+    $("#tally").html('randomNum: ' + randomNum);
+    for(var i = 0; i < 4; i++){
 
-    var value = Math.floor(Math.random() * 10) + 1;
-    // console.log(value);
+        var hiddenValue = Math.floor(Math.random() * 10) + 1;
+        // console.log(hiddenValue);
 
-    var rock = $("<div>");
-        rock.attr({
-            "class": 'rock',
-            "data-random": value
-        });
+        var rock = $("<div>");
+            rock.attr({
+                "class": 'rock',
+                "data-secret": hiddenValue
+            });
 
-    $(".crystals").append(rock);
+        $(".crystals").append(rock);
 
-}
+    }
+
+};
+
 
 $(".rock").on('click', function() {
-    console.log($(this));
+
+    // parsing num into integer
+    var num = parseInt($(this).attr('data-secret'));
+
+    // console.log(typeof num);
+
+    // tallying the hidden value of crystal clicked to players' current amount
+    oldValue += num;
+
+    console.log(oldValue);
+    // conditional logic checking on player's success
+    if(oldValue > randomNum){
+        // console.log("Oops! Try again...");
+
+        //increment the loss count
+        losses++;
+
+        //diplay the updated loss column
+        $("#losses").html(losses);
+
+    } else if(oldValue === randomNum){
+        // console.log("Lucky you!!")
+        
+        //increment win count
+        wins++;
+
+        //display the updated # of wins
+        $("#wins").html(wins);
+    };
+
 });
 
 
